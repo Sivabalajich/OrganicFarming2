daeb772e04d3099505969cce448d2955b1106d60
Siva Balaji Chenneboina <142612603+Sivabalajich@users.noreply.github.com>
2025-06-15 12:50:35 +0530

    Update NavFooter.js

--- NavFooter.js
+++ NavFooter.js
@@ -1,6 +1,6 @@
 const HEADER=`
 <header>
-    <div class="logo">Organic Farming 🌾   &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;</div>
+    <div class="logo">Organic Farming 🌾  </div>
     <nav>
       <ul id="nav-links">
         <li><a href="index.html">Home</a></li>
